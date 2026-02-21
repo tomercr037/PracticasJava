efcddc4e40a82ea5cbb346246745d958ecda9143
tomercr037 <tomercr037@gmail.com>
2026-02-21 15:58:06 -0600

    Se arreglo el tema de la edad

--- HolaMundo.java
+++ HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
         System.out.println("Hola Mundo");
         System.out.println("Saludos");
         System.out.println("Tomas de Jesus Segundo");
-        System.out.println("26 anios");
+        System.out.println("26 años");
         System.out.println("Mexico");
     //Fin metodo main
     }
